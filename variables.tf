0f426d684c70e2ae6452c97ab2c95d17b637c0d6
muhammed dhulkifli k <dhulkifilkanhirot@gmail.com>
2024-11-10 17:23:33 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "ami of ec2"
+  default     = "ami-0@#$%$#@##$#@"
+}
+
+variable "instance_type" {
+  description = "instace type of ec2"
+  default     = "t2.micro"
+}
+
+variable "key-pair" {
+  description = "value of the key-pair"
+  default     = "dk-key"
+}
